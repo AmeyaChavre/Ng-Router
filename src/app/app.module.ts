e1bcdc7dd7cf5556fec38087c0cc637bbfa5e773
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-15 14:25:57 +0530

    Commit#3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
@@ -9,6 +10,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+
+
+const appRoutes: Routes = [
+
+];
 
 @NgModule({
   declarations: [
