6808c1614331abd2174e648e1c7c07ae062609fc
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-15 14:40:25 +0530

    Commit#9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
