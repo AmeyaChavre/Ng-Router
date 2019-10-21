ffc8abbf2ca181fa1921ac4dc574b40a4bd826cd
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-21 13:45:56 +0530

    Commit#67

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RootGuardsComponent } from './root-guards/root-guards.component';
 
 
 const appRoutes: Routes = [
@@ -28,7 +29,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    RootGuardsComponent
   ],
   imports: [
     BrowserModule,
