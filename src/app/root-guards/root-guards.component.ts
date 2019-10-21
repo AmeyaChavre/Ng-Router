b1f9ffd635d07e70e417f0d7f87b1060a68e44a7
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-21 13:46:14 +0530

    Commit#68

---
+++ src/app/root-guards/root-guards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root-guards',
+  templateUrl: './root-guards.component.html',
+  styleUrls: ['./root-guards.component.css']
+})
+export class RootGuardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
