4c82ce8b318ff22488f1f2bc895aa5a8721b3782
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-23 12:46:36 +0530

    Commit#72

---
+++ src/app/middle-ware/middle-ware.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-middle-ware',
+  templateUrl: './middle-ware.component.html',
+  styleUrls: ['./middle-ware.component.css']
+})
+export class MiddleWareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
