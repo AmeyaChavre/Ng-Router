7fe9258e16e7194ee019522ba9abc7d96800d638
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-24 20:00:08 +0530

    Commit#74

---
+++ src/app/route-guard-block/route-guard-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-route-guard-block',
+  templateUrl: './route-guard-block.component.html',
+  styleUrls: ['./route-guard-block.component.css']
+})
+export class RouteGuardBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
