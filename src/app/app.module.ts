edb3245618856b9f75a9570b020ba4b616d05bf6
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-26 12:44:53 +0530

    Commit#78

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { InterfaceComponent } from './interface/interface.component';
 const appRoutes: Routes = [
  { path: '', component: HomeComponent },
  { path: 'users', component: UsersComponent },
- { path: 'users', component: UserComponent },
+ { path: 'users/:id', component: UserComponent },
  { path: 'servers', component: ServersComponent }
 ];
 
