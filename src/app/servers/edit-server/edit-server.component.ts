3844eb3dffd15d5d9b27a535ff39c03f47d7b17b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-27 11:34:06 +0530

    Commit#116

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -16,6 +16,7 @@ export class EditServerComponent implements OnInit {
     private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
