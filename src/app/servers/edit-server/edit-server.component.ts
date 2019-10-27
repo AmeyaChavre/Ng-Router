c4a5f75f1a82c22678524245f3db8d38fd26f19a
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-27 11:35:11 +0530

    Commit#119

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -17,7 +17,7 @@ export class EditServerComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
-    console.log();
+    console.log(this.route.snapshot.fragment);
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
