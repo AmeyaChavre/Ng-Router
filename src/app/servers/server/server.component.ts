57651078480d263656054a4ae9546fcb4002a0d2
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-28 14:31:13 +0530

    Commit#129

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/route';
+import { ActivatedRoute } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
