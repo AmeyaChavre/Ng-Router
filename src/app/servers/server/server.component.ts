ebd0fc239933e9b56dcd972102daa4c1ebd24508
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-28 14:36:00 +0530

    Commit#132

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -17,6 +17,12 @@ export class ServerComponent implements OnInit {
   ngOnInit() {
   	const id = this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
+  	this.route.params
+  		.subscribe(
+  			(params: Params) => {
+
+  			}
+  	);
   }
 
 }
