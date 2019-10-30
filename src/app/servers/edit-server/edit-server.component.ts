5484796acb1783329ebe22bfdd37a3e06645ad5e
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-30 12:56:39 +0530

    Commit#168

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -19,7 +19,8 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.fragment);
-    this.route.queryParams.subscribe(
+    this.route.queryParams
+    .subscribe(
         (queryParams: Params) => {
           this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
         }
