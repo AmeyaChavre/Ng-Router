983a683c4a5e5c1cc29bd40b270bb5462351441f
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-30 12:47:40 +0530

    Commit#159

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -21,7 +21,7 @@ export class EditServerComponent implements OnInit {
     console.log(this.route.snapshot.fragment);
     this.route.queryParams.subscribe(
         (queryParams: Params) => {
-          
+          this.allowEdit = queryParams['allowEdit'];
         }
     );
     this.route.fragment.subscribe();
