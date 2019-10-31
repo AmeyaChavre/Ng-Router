603212a2136541ce40154c5ad2c934ecea134f8b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-10-31 14:01:21 +0530

    Commit#185

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
 
-
 const appRoutes: Routes = [
  { path: '', component: HomeComponent },
  { path: 'users', component: UsersComponent , children: [
