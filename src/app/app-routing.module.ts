e107bbb1845e8453b9c83244b83679f15dcd9cc2
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-01 14:31:26 +0530

    Commit#214

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router'; 
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './users/user/user.component';
@@ -6,6 +7,7 @@ import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './servers/server/server.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 const appRoutes: Routes = [
  { path: '', component: HomeComponent },
