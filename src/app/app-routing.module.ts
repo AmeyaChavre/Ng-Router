007ec76135fa8636628853a0ac9e5908d3596eba
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-01 14:25:34 +0530

    Commit#206

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,20 @@
 import { NgModule } from '@angular/core';
 
+const appRoutes: Routes = [
+ { path: '', component: HomeComponent },
+ { path: 'users', component: UsersComponent , children: [
+                                              { path: ':id/:name', component: UserComponent }
+ ]},
+ { path: 'servers', component: ServersComponent , children: [
+                                                  { path: ':id', component: ServerComponent },
+                                                  { path: ':id/edit', component: EditServerComponent }
+ ]},
+
+ { path: 'not-found', component: PageNotFoundComponent }, // not-found route should be 
+ { path: '**', redirectTo: '/not-found'} // mentioned in the end as routes get parsed from top to bottom
+
+];
+
 @NgModule({
 
 })
