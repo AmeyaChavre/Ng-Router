88ec5b0b04d9b833ec589d97e3473842a434cb28
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-01 14:27:20 +0530

    Commit#209

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component'
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
 
 const appRoutes: Routes = [
  { path: '', component: HomeComponent },
