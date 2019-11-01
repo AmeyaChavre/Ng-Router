a3dd92b0a96a2cff07da0404affc7a90695a3f15
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-01 14:35:34 +0530

    Commit#217

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './app-routing.module.ts'; 
+import { AppRoutingModule } from './app-routing.module'; 
 
 
 
