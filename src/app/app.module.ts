e83b5f925e5346f22d56dbd8aba75f5bed65de4b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-01 14:36:20 +0530

    Commit#218

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
