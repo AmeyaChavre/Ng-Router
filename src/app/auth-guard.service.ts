ad82a0142ed6bf745a06e42384abd8089d8d8123
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-03 16:19:36 +0530

    Commit#264

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,4 @@
-import 
-{ CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot,Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot,Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable'; 
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
