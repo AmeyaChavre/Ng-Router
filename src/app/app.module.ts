36c8b517a6354cc0873ccd6c69321c40abea17a2
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-03 15:15:22 +0530

    Commit#260

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module'; 
-import { AuthService } from '';
+import { AuthService } from './auth.service';
 import { AuthGuard } from '';
 
 
