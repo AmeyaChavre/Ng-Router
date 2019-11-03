a8a3c2877695ae170667b1f5eaed1027073cb92a
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-03 15:08:54 +0530

    Commit#251

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const appRoutes: Routes = [
  { path: 'users', component: UsersComponent , children: [
                                               { path: ':id/:name', component: UserComponent }
  ]},
- { path: 'servers', component: ServersComponent , children: [
+ { path: 'servers', canActivate: [], component: ServersComponent , children: [
                                                   { path: ':id', component: ServerComponent },
                                                   { path: ':id/edit', component: EditServerComponent }
  ]},
