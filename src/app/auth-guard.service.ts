e410fac6db68924d328cea537762191b076af5cd
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-03 15:00:10 +0530

    Commit#247

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -13,6 +13,8 @@ import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+	constructor(private authService: AuthService, private router: Router){}
+
 	canActivate(route: ActivatedRouteSnapshot, 
 		state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 		this.authService.isAuthenticated()
