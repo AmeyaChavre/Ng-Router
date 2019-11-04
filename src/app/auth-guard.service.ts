55b57ed1b9bdbab9eaae23fb165fc9f71dacddd5
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-04 11:11:06 +0530

    Commit#270

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -29,6 +29,8 @@ export class AuthGuard implements CanActivate,CanActivateChild {
  }
 
  canActivateChild(route: ActivatedRouteSnapshot, 
-				state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean)
+				state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean) {
+	
+}
 
 }
