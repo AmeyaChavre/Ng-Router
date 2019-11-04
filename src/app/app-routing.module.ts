e78e6c37aaedbcc38dc1569fccdac24de4a75dd1
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-04 11:30:47 +0530

    Commit#275

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const appRoutes: Routes = [
                                               { path: ':id/:name', component: UserComponent }
  ]},
  { path: 'servers', 
- canActivate: [AuthGuard], 
+ //canActivate: [AuthGuard], 
  component: ServersComponent , 
  children: [
 	{ path: ':id', component: ServerComponent },
