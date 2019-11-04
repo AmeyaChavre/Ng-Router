758f6c2d3e56dfafe597a5960dc14af6134352d2
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-04 11:45:39 +0530

    Commit#285

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,4 +18,8 @@ export class HomeComponent implements OnInit {
   	this.router.navigate(['/servers', id, 'edit'], { queryParams: {allowEdit: '1'}, fragment: "loading" });
   }
 
+  onLogin(){
+  	
+  }
+
 }
