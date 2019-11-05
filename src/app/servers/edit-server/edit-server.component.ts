e3e6064211006104571e6aec94b4164ba30128e7
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-05 12:46:33 +0530

    Commit#316

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -31,7 +31,7 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
     );
     this.route.fragment.subscribe();
     const id = this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
