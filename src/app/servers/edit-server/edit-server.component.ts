6a3c4b06ea00f35e6105d092d660e4d20c2e5f31
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-05 11:58:25 +0530

    Commit#291

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -15,7 +15,8 @@ export class EditServerComponent implements OnInit {
   changesSaved=false;
 
   constructor(private serversService: ServersService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
