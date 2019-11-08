aefd2c31bed5c10686bd63a857a3868051b9d4d7
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-08 13:55:24 +0530

    Commit#349

--- src/app/servers/server/server-resolver.service.ts
+++ src/app/servers/server/server-resolver.service.ts
@@ -1,9 +1,12 @@
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
+interface Server {
+	id: number,
+}
 
 export class ServerResolver implements Resolve<{id: number, name:string ,status:string}> {
-	resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable{
+	resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable <> {
 
 	}
 }
