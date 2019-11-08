5a96d5431d0c8ee33a23e1237f64cb0c7a5369b7
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-08 13:51:48 +0530

    Commit#343

--- src/app/servers/server/server-resolver.service.ts
+++ src/app/servers/server/server-resolver.service.ts
@@ -1,5 +1,7 @@
 import { Resolve } from '@angular/router';
 
 export class ServerResolver implements Resolve<{id: number, name:string ,status:string}> {
-
+	resolve(route: ActivatedRouteSnapshot){
+		
+	}
 }
