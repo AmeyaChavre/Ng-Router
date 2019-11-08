3be79d44e9e2ad7dab4da7e1ac52aa739897c496
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-11-08 14:01:57 +0530

    Commit#358

--- src/app/servers/server/server-resolver.service.ts
+++ src/app/servers/server/server-resolver.service.ts
@@ -1,11 +1,15 @@
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
+import { ServersService } from '../servers.service';
+
+
 interface Server {
 	id: number;
 	name: string;
 	status: string
 }
+
 
 export class ServerResolver implements Resolve<Server> {
 	constructor(private serversService: ServersService) {}
